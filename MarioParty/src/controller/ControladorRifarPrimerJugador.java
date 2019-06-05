eeffb12f5c0f3f8085a5b503203a4dd08d207b75
Esteban Cruz <xllestebanllx@gmail.com>
2019-06-05 12:02:51 -0600

    Ventana rifar primer jugador

--- MarioParty/src/controller/ControladorRifarPrimerJugador.java
+++ MarioParty/src/controller/ControladorRifarPrimerJugador.java
@@ -11,27 +11,35 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
  *
  * @author zEstebanCruz
  */
-public class ControladorEscogerJugadores implements Initializable {
+public class ControladorRifarPrimerJugador implements Initializable {
 
 	@FXML
-	private ComboBox<Integer> comboBoxCantJugadores;
+	private ImageView imagenPersonajeActual;
 	@FXML
-	private Button botonListoxd;
+	private Label labelNumeroGenerado;
+	@FXML
+	private Label labelNumeroAObtener;
+	@FXML
+	private Button botonGenerarAleatorio;
 
+	/**
+	 * Initializes the controller class.
+	 */
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		// TODO
 	}
 
 	@FXML
-	private void accion(ActionEvent event) {
+	private void gernerarAleatorio(ActionEvent event) {
 	}
 
 }
